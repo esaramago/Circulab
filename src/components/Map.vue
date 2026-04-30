304b1b94165eb4e94c9dc056e646c45673bf8953
Emanuel Saramago <dev@emanuelsaramago.com>
2026-04-30 18:28:55 +0100

    markers popup

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,13 +1,43 @@
 <template>
   <div id="map"></div>
+  <MarkerPopup :open="activeMarker !== null" :marker="activeMarker" @close="activeMarker = null" />
 </template>
 
-<script setup>
-import { onMounted } from 'vue';
-import { Map, TileLayer, Marker, Popup } from 'leaflet'
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import { Map, TileLayer, Marker, LayerGroup, DivIcon } from 'leaflet'
+import type { Database } from '@/types/supabase'
 import 'leaflet/dist/leaflet.css'
-//import 'leaflet.markercluster/dist/MarkerCluster.css'
-//import 'leaflet.markercluster/dist/MarkerCluster.Default.css'
+import MarkerPopup from '@/components/MarkerPopup.vue'
+type MarkerType = Database['public']['Tables']['pins']['Row']
+
+const props = defineProps<{
+  markers: MarkerType[]
+}>()
+
+const activeMarker = ref<MarkerType | null>(null)
+
+const tree = {}
+function ensureGroup(typology: MarkerType['category_id'], category: MarkerType['category_id'], characteristics: MarkerType['characteristics_ids']) {
+  tree[typology] ??= {}
+  tree[category_id][characteristics_ids] ??= new LayerGroup()
+  return tree[category_id][characteristics_ids]
+}
+
+const tooltipSize = 24;
+const tooltipAnchor = tooltipSize / 2;
+
+const pin = new DivIcon({
+  html: `<div class="c-marker">O</div>`,
+  iconSize: [tooltipSize, tooltipSize],
+  iconAnchor: [tooltipAnchor, tooltipAnchor]
+})
+
+const tooltipOptions = {
+  direction: 'top',
+  offset: [0, -(tooltipAnchor / 1.5)],
+  className: 'c-tooltip'
+}
 
 onMounted(() => {
   const map = new Map('map', {
@@ -20,7 +50,34 @@ onMounted(() => {
     maxNativeZoom: 19,
     maxZoom: 22,
   }).addTo(map)
-});
+
+  //const markersLayer = new LayerGroup()
+  //markersLayer.addTo(map)
+  addMarkers(props.markers, map/* , markersLayer */)
+})
+
+function addMarkers(markers, map) {
+  markers.forEach(marker => {
+    //const group = ensureGroup(marker.typology, marker.category, marker.characteristics)
+    //group.addTo(map)
+    addMarker(marker, map)
+  })
+}
+function addMarker(marker, map) {
+  const markerLayer = new Marker([
+    marker.coordinates.latitude,
+    marker.coordinates.longitude],
+    { icon: pin })
+  .addTo(map).bindTooltip(marker.title, tooltipOptions)
+
+  markerLayer.on('click', () => {
+    showPopup(marker)
+  })
+}
+
+function showPopup(marker) {
+  activeMarker.value = marker // set active marker
+}
 </script>
 
 <style scoped>
@@ -29,6 +86,49 @@ onMounted(() => {
   overflow: hidden;
   @media (min-width: 768px) {
     border-radius: var(--wa-border-radius-l);
+  }
+}
+</style>
+
+<style>
+.leaflet-container {
+  font-family: inherit;
+}
+.leaflet-marker-icon {
+  border: none !important;
+  background-color: transparent !important;
+}
+.c-marker {
+  height: 100%;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: var(--wa-color-brand-95);
+  border: 2px solid var(--wa-color-brand-10);
+  box-sizing: border-box;
+  border-radius: var(--wa-border-radius-circle);
+  font-size: var(--wa-font-size-s);
+  font-weight: var(--wa-font-weight-semibold);
+  text-align: center;
+  line-height: 1;
+  transition: transform 160ms ease;
+  &:hover {
+    transform: scale(1.2);
+  }
+}
+.c-tooltip {
+  padding: var(--wa-space-2xs) var(--wa-space-xs);
+  font-size: var(--wa-font-size-xs);
+  background-color: #FFF;
+  line-height: 1;
+  opacity: 1 !important;
+  font-weight: var(--wa-font-weight-semibold);
+  border-color: var(--wa-color-neutral-90);
+  border-radius: var(--wa-border-radius-m);
+  box-shadow: var(--wa-shadow-m);
+  &::before {
+    display: none;
   }
 }
 </style>
---
+++ src/components/MarkerPopup.vue
@@ -0,0 +1,32 @@
+<template>
+  <wa-drawer
+    id="marker-popup"
+    :label="marker?.title"
+    :open="open"
+    @wa-hide="emit('close')"
+  >
+    <p>{{ marker?.description }}</p>
+  </wa-drawer>
+</template>
+
+<script setup lang="ts">
+import type { Database } from '@/types/supabase'
+
+type MarkerType = Database['public']['Tables']['pins']['Row']
+
+const props = defineProps<{
+  marker: MarkerType,
+  open: boolean
+}>()
+
+const emit = defineEmits<{
+  (e: 'close'): void
+}>()
+
+</script>
+
+<style scoped>
+#marker-popup {
+  --size: clamp(48rem, 30vw, 80rem);
+}
+</style>
